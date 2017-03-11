9468abdaebc6ea9b5b6289c84b79c0bbda7c8ba7
leptone <leptone55@gmail.com>
2017-03-11 10:54:26 -0800

    inital commit

---
+++ ReadMes/tools.md
@@ -0,0 +1,12 @@
+Development
+- React
+- Redux
+- Express.js
+- MongoDB
+
+Architecture
+- Isomorphic
+
+Dev Ops
+- Serverless + Lambda
+- S3
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+let webpack = require('webpack')
+
+module.exports = {
+    entry: [
+        'webpack-hot-middleware/client',
+        './client/client.js'
+    ],
+    output: {
+        path: require("path").resolve("./dist"),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devtool: 'source-maps',
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
+                    plugins: ['transform-class-properties', 'transform-decorators-legacy']
+                }
+            }
+        ]
+    }
+}
